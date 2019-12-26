2fd78e430a8ad2aa0dd0c8b60476e9147712ab4b
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-12-26 17:31:36 +0530

    26th

--- DemoProject/src/demopack/DemoRadio.java
+++ DemoProject/src/demopack/DemoRadio.java
@@ -8,16 +8,17 @@ public class DemoRadio {
 
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new FirefoxDriver();
-		//FirefoxDriver driver = new FirefoxDriver();
 		driver.get("https://krninformatix.com/sample.html");
-        
 		boolean status=driver.findElement(By.id("female")).isSelected();
 		System.out.println(status);
 		
 		Thread.sleep(3000);
 		driver.findElement(By.id("female")).click();
+		
 		boolean status1 = driver.findElement(By.id("female")).isSelected();
 		System.out.println(status1);
+		
+		
 	}
 
 }
